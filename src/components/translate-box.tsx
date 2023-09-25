cb05ff7e61c25e4183d93ac08541f8f7101c4fb8
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-09-25 05:34:52 -0300

    add translate box

---
+++ src/components/translate-box.tsx
@@ -0,0 +1,57 @@
+import { ChangeEvent, useState, memo } from "react";
+import { useLanguageContext } from "../context/language-context";
+
+import { Textarea } from "@nextui-org/react";
+import { IoClose } from "react-icons/io5";
+
+import { translateText } from "../utils/translateText";
+
+export const TranslateBox = memo(() => {
+  const { sourceLanguage, targetLanguage, text, setText, setTranslatedText } =
+    useLanguageContext();
+
+  const [wordCounter, setWordCounter] = useState(0);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setWordCounter(e.target.value.length);
+    setText(e.target.value);
+  };
+
+  const handleReset = () => {
+    setText("");
+    setWordCounter(0);
+  };
+
+  return (
+    <>
+      <section className="p-4 bg-[#27272A] rounded-md">
+        <div className="flex gap-4 items-center">
+          <Textarea
+            autoCorrect="off"
+            autoComplete="off"
+            minRows={1}
+            placeholder="type to translate"
+            onChange={handleChange}
+            type="text"
+            maxLength={5000}
+            value={text}
+          />
+          <IoClose onClick={handleReset} className="text-xl" />
+        </div>
+
+        <div className="flex justify-end text-sm text-[#A1A1AA]">
+          <p>{wordCounter}/5000</p>
+        </div>
+      </section>
+
+      <button
+        onClick={() =>
+          translateText(text, targetLanguage, sourceLanguage, setTranslatedText)
+        }
+        className="w-full p-4 mt-2 rounded-xl"
+      >
+        translate
+      </button>
+    </>
+  );
+});
